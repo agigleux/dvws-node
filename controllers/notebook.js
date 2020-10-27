5d83fcf3394084c064d2e213d3f20e2dde2de3f0
Alexandre Gigleux <alexandre.gigleux@gmail.com>
2020-10-27 11:45:02 +0100

    Update notebook.js

--- controllers/notebook.js
+++ controllers/notebook.js
@@ -27,7 +27,7 @@ module.exports = {
         const token = req.headers.authorization.split(' ')[1]; // Bearer <token>
         const options = {
           expiresIn: '2d',
-          algorithm: 'HS256'
+          algorithms: 'HS256'
         };
         result = jwt.verify(token, process.env.JWT_SECRET, options);
         Note.find({ user: result.user }, { __v: 0 }, function (err, someValue) {
